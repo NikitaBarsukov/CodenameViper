b853e6fe25ba4b0447d10a6bfd04b99ca56fb4ad
Nikita Barsukov <nik0xff@gmail.com>
2019-12-26 17:53:50 +0300

    new

--- src/main/java/com/FileController.java
+++ src/main/java/com/FileController.java
@@ -2,12 +2,18 @@ package com;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.apache.commons.io.IOUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 @RestController
 public class FileController {
 
-    @RequestMapping("/")
-    public String index() {
-        return "The swift file hello";
+    @RequestMapping("/doit")
+    public byte[] index() throws IOException {
+        InputStream in = getClass()
+                .getResourceAsStream("hello.swift");
+        return IOUtils.toByteArray(in);
     }
 }
---
+++ src/test/java/com/FileControllerTest.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FileControllerTest {
+
+    @Test
+    void index() {
+        assertEquals(1, 1);
+    }
+}
